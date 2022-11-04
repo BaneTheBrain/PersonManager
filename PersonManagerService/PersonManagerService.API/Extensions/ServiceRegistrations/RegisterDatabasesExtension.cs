f6ba3c7e10a358efd3c9e43f0589579f829ce718
bni <br4nislavnikolic@gmail.com>
2022-11-04 16:21:58 +0100

    Seed in memory db

--- PersonManagerService/PersonManagerService.API/Extensions/ServiceRegistrations/RegisterDatabasesExtension.cs
+++ PersonManagerService/PersonManagerService.API/Extensions/ServiceRegistrations/RegisterDatabasesExtension.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using PersonManagerService.Domain.Models;
 using PersonManagerService.Infrastructure.Contexts;
 
 namespace PersonManagerService.API.Extensions.ServiceRegistrations;
@@ -15,8 +16,37 @@ public static class RegisterDatabasesExtension
         }
         else
         {
-            serviceCollection.AddDbContext<PersonManagerServiceDbContext>(c => c.UseInMemoryDatabase("PersonDbInMemory"));
-            //to do: seed in memory db
+            var optionsBuilder = new DbContextOptionsBuilder<PersonManagerServiceDbContext>().UseInMemoryDatabase(databaseName: "PersonDbInMemory");
+            using (var context = new PersonManagerServiceDbContext(optionsBuilder.Options))
+            {
+                var personAId = new Guid("a5d45942-98b8-42e9-9e13-1718fb954fd1");
+                var personBId = new Guid("4c0ff862-187e-4b7c-8e06-44f08e613d8f");
+                var fbAccountId = new Guid("acd7419f-73eb-48ef-b042-05de4bb7dc8c");
+                var twitterAccountId = new Guid("86d3674f-6778-4cc6-a593-55e7f5c4dd8b");
+
+                context.Persons.Add(new Person { PersonId = personAId, FirstName = "Pera", LastName = "Zdera" });
+                context.Persons.Add(new Person { PersonId = personBId, FirstName = "Mitar", LastName = "Miric" });
+
+
+                context.SocialMediaAccounts.Add( new SocialMediaAccount { SocialMediaAccountId = fbAccountId, Type = "Facebook" });
+                context.SocialMediaAccounts.Add(new SocialMediaAccount { SocialMediaAccountId = twitterAccountId, Type = "Twitter" });
+                context.SocialMediaAccounts.Add(new SocialMediaAccount { SocialMediaAccountId = Guid.NewGuid(), Type = "LinkedIn" });
+                context.SocialMediaAccounts.Add(new SocialMediaAccount { SocialMediaAccountId = Guid.NewGuid(), Type = "Telegram" });
+
+
+                context.PersonSkills.Add(new PersonSkill { PersonId = personAId, Name = "debeo", PersonSkillId = Guid.NewGuid() });
+                context.PersonSkills.Add(new PersonSkill { PersonId = personAId, Name = "spor", PersonSkillId = Guid.NewGuid() });
+                context.PersonSkills.Add(new PersonSkill { PersonId = personBId, Name = "brz", PersonSkillId = Guid.NewGuid() });
+                context.PersonSkills.Add(new PersonSkill { PersonId = personBId, Name = "peva", PersonSkillId = Guid.NewGuid()});
+
+                context.PersonSocialMediaAccounts.Add( new PersonSocialMediaAccount { PersonId = personAId, SocialMediaAccountId = fbAccountId, Address = "pera@fb" });
+                context.PersonSocialMediaAccounts.Add(new PersonSocialMediaAccount { PersonId = personBId, SocialMediaAccountId = fbAccountId, Address = "mita@fb" });
+                context.PersonSocialMediaAccounts.Add(new PersonSocialMediaAccount { PersonId = personBId, SocialMediaAccountId = twitterAccountId, Address = "mita@tw" });
+
+                context.SaveChanges();
+            }
+
+            serviceCollection.AddDbContext<PersonManagerServiceDbContext>(c => c.UseInMemoryDatabase(databaseName: "PersonDbInMemory"));
         }
     }
 }
